a9953b6d219a6875ee599774bfc01733ce1e1855
debbiebrozzoski <debbie.brozzoski@gmail.com>
2020-08-07 01:20:00 -0400

    debbies splash page

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,59 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.css";
+import React, {useEffect, useState} from "react";
 import './App.css';
+import TodoForm from "./Splash/TodoForm";
+import TodoList from "./Splash/TodoList"
+import Typography from "@material-ui/core/Typography";
+import Logo from "./assets/logo.png";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      items:[ ],
-      currentItem:{
-        text: ' ',
-        key: ' '
-      }
+
+function App( ) {
+  const [todos, setTodos] = useState([ ]) ;
+
+  useEffect(( ) => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if  (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, [])
+
+  useEffect(( ) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo) {
+    setTodos([ todo, ...todos]);
   }
-  this.handleInput = this.handleInput.bind(this);
-  this.addItem = this.addItem.bind(this);
-}
-handleInput(e) {
-  this.setState ( {
-    currentItem: {
-      text: e.target.value,
-      key: Date.now( )
+
+  function toggleComplete( id ) {
+    setTodos(
+      todos.map( todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      } )
+    );
   }
-})
-}
 
-addItem (e ) {
-  e.preventDefault( );
-  const newItem = this.state.currentItem;
-  console.log(newItem);
-}
+  function removeTodo ( id ) {
+    setTodos(todos.filter( todo => todo.id !== id ));
 
-  render ( ) {
-    return (
-      <div className="App">
-      <header> 
-        <form id="to-do-form" onSubmit={this.addItem}>
-              <input type="text" placeholder="Enter Text" 
-                value={this.state.currentItem.text}
-                onChange={this.handleInput}/>
-          <button type="submit">Add</button>
-        </form>
-      </header>
-      </div>
-    )}
+  }
+
+  return (
+    <div className="App">
+        <Typography style={{ padding: 16 }}variant="h1">List Buddy</Typography>
+        <TodoForm addTodo={addTodo}/>
+        <TodoList todos={todos} toggleComplete={toggleComplete} 
+        removeTodo={ removeTodo} />
+    </div>
+  );
 }
 
 export default App;
-
-
-
