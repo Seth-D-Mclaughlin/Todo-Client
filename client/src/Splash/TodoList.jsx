7de94a2a037cce5efc4cfb935a60d8450239a571
debbiebrozzoski <debbie.brozzoski@gmail.com>
2020-08-07 14:05:27 -0400

    fixed error on TodoForm

--- client/src/Splash/TodoList.jsx
+++ client/src/Splash/TodoList.jsx
@@ -1,21 +1,19 @@
 import { List } from "@material-ui/core";
-import React from 'react';
+import React from "react";
 import Todo from "./Todo";
 
-
-
-function TodoList ({ todos, toggleComplete, removeTodo }) {
-    return (
-        <List>
-            {todos.map(todo =>(
-                <Todo 
-                key={ todo.id } 
-                todo={todo} 
-                toggleComplete={ toggleComplete }
-                removeTodo={removeTodo}/>
-            ))}
-        </List>
-    );
+function TodoList({ todos, removeTodo, toggleComplete }) {
+  return (
+    <List>
+      {todos.map(todo => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          removeTodo={removeTodo}
+          toggleComplete={toggleComplete}/>
+      ))}
+    </List>
+  );
 }
 
 export default TodoList;
