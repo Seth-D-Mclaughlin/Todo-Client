61b681add5f694c65549c07853a577a00ef7232c
debbiebrozzoski <debbie.brozzoski@gmail.com>
2020-08-10 10:16:24 -0400

    splash2.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,19 @@
-import Typography from "@material-ui/core/Typography";
-import React, { useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import "./App.css";
-import TodoForm from "./Splash/TodoForm";
-import TodoList from "./Splash/TodoList";
 
-const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+//components
+
+import InputTodo from "./Splash2/Components/InputTodo";
+import ListTodos from "./Splash2/Components/ListTodos";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    // fires when app component mounts to the DOM
-    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storageTodos) {
-      setTodos(storageTodos);
-    }
-  }, []);
-
-  useEffect(() => {
-    // fires when todos array gets updated
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]);
-
-  function addTodo(todo) {
-    // adds new todo to beginning of todos array
-    setTodos([todo, ...todos]);
-  }
-
-  function toggleComplete(id) {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed
-          };
-        }
-        return todo;
-      })
-    );
-  }
-
-  function removeTodo(id) {
-    setTodos(todos.filter(todo => todo.id !== id));
-  }
-
   return (
-    <div className="App">
-      <Typography style={{ padding: 16 }} variant="h1">
-        List Buddy
-        
-      </Typography>
-      <TodoForm addTodo={addTodo} />
-      <TodoList
-        todos={todos}
-        removeTodo={removeTodo}
-        toggleComplete={toggleComplete}
-      />
-    </div>
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </Fragment>
   );
 }
 
--- client/src/Splash/TodoForm.jsx
+++ client/src/Splash/TodoForm.jsx
@@ -1,41 +1,41 @@
-import { Button, TextField } from "@material-ui/core";
-import React, { useState } from "react";
-import {v4 as uuid} from "uuid"; 
+// import { Button, TextField } from "@material-ui/core";
+// import React, { useState } from "react";
+// import {v4 as uuid} from "uuid";
 
-function TodoForm({ addTodo }) {
-  const [todo, setTodo] = useState({
-    id: "",
-    task: "",
-    completed: false
-  });
+// function TodoForm({ addTodo }) {
+//   const [todo, setTodo] = useState({
+//     id: "",
+//     task: "",
+//     completed: false
+//   });
 
-  function handleTaskInputChange(e) {
-    // e.target.value contains new input from onChange
-    // event for input elements
-    setTodo({ ...todo, task: e.target.value });
-  }
+//   function handleTaskInputChange(e) {
+//     // e.target.value contains new input from onChange
+//     // event for input elements
+//     setTodo({ ...todo, task: e.target.value });
+//   }
 
-  function handleSubmit(e) {
-    e.preventDefault(); // prevents browser refresh
-    // trim() gets rid of string whitespace
-    if (todo.task.trim()) {
-      addTodo({ ...todo, id: uuid() });
-      setTodo({ ...todo, task: "" });
-    }
-  }
+//   function handleSubmit(e) {
+//     e.preventDefault( ); // prevents browser refresh
+//     // trim() gets rid of string whitespace
+//     if (todo.task.trim( )) {
+//       addTodo({ ...todo, id: uuid( ) });
+//       setTodo({ ...todo, task: "" });
+//     }
+//   }
 
-  return (
-    <form className="todo-form" onSubmit={handleSubmit}>
-      <TextField
-        label="Task"
-        type="text"
-        name="task"
-        value={todo.task}
-        onChange={handleTaskInputChange}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
-  );
-}
+//   return (
+//     <form className="todo-form" onSubmit={handleSubmit}>
+//       <TextField
+//         label="Task"
+//         type="text"
+//         name="task"
+//         value={todo.task}
+//         onChange={handleTaskInputChange}
+//       />
+//       <Button type="submit">Submit</Button>
+//     </form>
+//   );
+// }
 
-export default TodoForm;
+// export default TodoForm;
