c637b36c5d3aba520ab5af924f4b559f01885637
Cameron Benson <66973535+bensonecameron@users.noreply.github.com>
2020-08-13 18:11:53 -0400

    heroku

--- client/src/helpers/environment.js
+++ client/src/helpers/environment.js
@@ -1,10 +1,12 @@
 let APIURL ='';
 
 switch (window.location.hostname) {
+  //this is the local host name of your react app
   case 'localhost' || '127.0.0.1':
+    // this is the localhost name of your API
     APIURL = 'http://localhost:3001'
     break;
-
+    // this is the deployed react application
     case 'todo-client-deploy.herokuapp.com':
       APIURL = 'https://dashboard.heroku.com/apps/efa-wd-listbuddy'
 }
